e15224bf839c14c631b7982077d2fb7303674489
arna9201 <anra9201@naver.com>
2023-05-15 00:44:56 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,7 +17,16 @@ const loginFn = () => {
         },
         body : JSON.stringify(req),
     }).then((res) => res.json()
-    ).then(console.log);
+    ).then((res) => {
+      if(res.success == true){
+        location.href = '/';
+      }else{
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+        console.error(new Error('로그인 중 에러 발생'));
+    })
 }
 
 button.addEventListener('click',loginFn);
